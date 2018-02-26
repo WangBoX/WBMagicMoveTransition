1fc158ea3283c2b0c03ac9bb6586e684327e60c9
WangBoX <563915318@qq.com>
2018-02-26 15:48:20 +0800

    项目初始化

---
+++ WBMagicMoveTransition/WBMagicMoveTransition/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WBMagicMoveTransition
+//
+//  Created by 王博Bling on 2018/2/26.
+//  Copyright © 2018年 王博. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
